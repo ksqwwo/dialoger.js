54b0b5508529c8771c2a3b81ada9b97c1bb91ba8
iredoff <64153930+iredoff@users.noreply.github.com>
2020-04-22 23:30:02 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# dialoger.js
-Package for creating a dialogue with a virtual girl(xu.su)
+# dialoger.js - Fast, convenient chat!
+
+* [Discord](https://discord.gg/eYuggsY)
+
+* [First Github Account](https://github.com/perssBest)
+
+#### **Важно знать!**
+
++ Для работы данного пакеты необходим другой пакет - `node-fetch`
+
++ Данный пакет поддерживает только русский язык.
+
+### Использование:
+
+```js
+const dialog = require('dialoger.js')
+const itog = await dialog.start('Your-Message')
+
+console.log(itog.text) // Получаем ответ от апи..
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.start = function(message){
+    if(!message) return 'Enter your message. Help --> https://discord.gg/eYuggsY'
+    return new Promise((resolve, reject) => {
+        let content = { 
+            bot: 'main', 
+            text: message 
+        }
+        fetch('http://xu.su/api/send', {
+            body: JSON.stringify(content),
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            mode: 'cors',
+        }).then(res => res.json()).then(json => {
+            resolve(json)
+        })
+    })
+}
+
+exports.version = require('./package.json').version
